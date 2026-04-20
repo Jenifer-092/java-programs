ff8902741fe5ecef45029df7eef13bbe3a75a36d
Jenifer DSouza <Jenifer DSouza@192.168.23.207>
2026-04-20 18:50:51 +0530

    commit

---
+++ src/main/java/com/cookie/CookieServlet.java
@@ -0,0 +1,57 @@
+package com.cookie;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/cookie")
+public class CookieServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("username");
+        int visitCount = 1;
+
+        // Check existing cookies
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (Cookie c : cookies) {
+                if (c.getName().equals("visitCount")) {
+                    visitCount = Integer.parseInt(c.getValue());
+                    visitCount++;
+                }
+            }
+        }
+
+        // Create cookies
+        Cookie nameCookie = new Cookie("username", name);
+        Cookie countCookie = new Cookie("visitCount", String.valueOf(visitCount));
+
+        // Set expiry (30 seconds for demo)
+        nameCookie.setMaxAge(30);
+        countCookie.setMaxAge(30);
+
+        response.addCookie(nameCookie);
+        response.addCookie(countCookie);
+
+        // Output
+        out.println("<html><body>");
+        out.println("<h2>Welcome back " + name + "!</h2>");
+        out.println("<p>You have visited this page " + visitCount + " times</p>");
+        out.println("<p><b>Note:</b> Cookie will expire in 30 seconds</p>");
+        out.println("<a href='index.html'>Go Back</a>");
+        out.println("</body></html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("index.html");
+    }
+}
